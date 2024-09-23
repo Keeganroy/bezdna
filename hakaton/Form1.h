daf98e4cd16041daa93dc01e52ca7c1e7409bbb3
lilloli <ghghghgg11@mail.ru>
2024-09-23 20:29:12 +0600

    add checkbox

--- hakaton/Form1.h
+++ hakaton/Form1.h
@@ -39,6 +39,7 @@ namespace CppCLRWinFormsProject {
 	private: System::Windows::Forms::Button^ button2;
 	private: System::Windows::Forms::Button^ button3;
 	private: System::Windows::Forms::Label^ title;
+	private: System::Windows::Forms::CheckBox^ checkBox1;
 
 	private:
 		/// <summary>
@@ -57,13 +58,15 @@ namespace CppCLRWinFormsProject {
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->button3 = (gcnew System::Windows::Forms::Button());
 			this->title = (gcnew System::Windows::Forms::Label());
+			this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(492, 179);
+			this->button1->Location = System::Drawing::Point(738, 275);
+			this->button1->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(177, 32);
+			this->button1->Size = System::Drawing::Size(266, 49);
 			this->button1->TabIndex = 0;
 			this->button1->Text = L"button1";
 			this->button1->UseVisualStyleBackColor = true;
@@ -71,18 +74,20 @@ namespace CppCLRWinFormsProject {
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(492, 217);
+			this->button2->Location = System::Drawing::Point(738, 334);
+			this->button2->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(177, 32);
+			this->button2->Size = System::Drawing::Size(266, 49);
 			this->button2->TabIndex = 1;
 			this->button2->Text = L"button2";
 			this->button2->UseVisualStyleBackColor = true;
 			// 
 			// button3
 			// 
-			this->button3->Location = System::Drawing::Point(492, 255);
+			this->button3->Location = System::Drawing::Point(738, 393);
+			this->button3->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
 			this->button3->Name = L"button3";
-			this->button3->Size = System::Drawing::Size(177, 32);
+			this->button3->Size = System::Drawing::Size(266, 49);
 			this->button3->TabIndex = 2;
 			this->button3->Text = L"button3";
 			this->button3->UseVisualStyleBackColor = true;
@@ -94,22 +99,35 @@ namespace CppCLRWinFormsProject {
 			this->title->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::World,
 				static_cast<System::Byte>(204)));
 			this->title->ForeColor = System::Drawing::SystemColors::ButtonHighlight;
-			this->title->Location = System::Drawing::Point(514, 128);
+			this->title->Location = System::Drawing::Point(771, 197);
+			this->title->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->title->Name = L"title";
 			this->title->Size = System::Drawing::Size(123, 24);
 			this->title->TabIndex = 3;
 			this->title->Text = L"First commit";
 			this->title->Click += gcnew System::EventHandler(this, &Form1::title_Click);
 			// 
+			// checkBox1
+			// 
+			this->checkBox1->AutoSize = true;
+			this->checkBox1->Location = System::Drawing::Point(479, 230);
+			this->checkBox1->Name = L"checkBox1";
+			this->checkBox1->Size = System::Drawing::Size(113, 24);
+			this->checkBox1->TabIndex = 4;
+			this->checkBox1->Text = L"checkBox1";
+			this->checkBox1->UseVisualStyleBackColor = true;
+			// 
 			// Form1
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1143, 636);
+			this->ClientSize = System::Drawing::Size(1714, 978);
+			this->Controls->Add(this->checkBox1);
 			this->Controls->Add(this->title);
 			this->Controls->Add(this->button3);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
+			this->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
 			this->Name = L"Form1";
 			this->Text = L"Form1";
 			this->Load += gcnew System::EventHandler(this, &Form1::Form1_Load);
@@ -121,7 +139,7 @@ namespace CppCLRWinFormsProject {
 	private: System::Void Form1_Load(System::Object^ sender, System::EventArgs^ e) {
 	}
 	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
-
+	
 	}
 	private: System::Void title_Click(System::Object^ sender, System::EventArgs^ e) {
 	}
